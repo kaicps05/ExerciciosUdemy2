677c4f14409c8edc37bcd27eb64f98037566dc5c
kaicps05 <kaicsalha@icloud.com>
2026-03-13 14:21:08 -0300

    melhorando o calculo da area

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() {
+        double p = (a + b + c) / 2 ;
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return p;
+    }
+
 }
