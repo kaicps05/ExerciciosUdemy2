b5f4a5ea3cfc151a250a3fe94608f24a8757ce1e
Kaic <kaicsalha@icloud.com>
2026-03-12 22:49:16 -0300

    Implement triangle area calculation in main method
    Refactored the main method to calculate areas of two triangles based on user input.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
+import java.util.Scanner;
+import java.util.Locale;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        Triangle x,y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Digite a medida do triangulo X : ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("Digite a medida do trianglo Y : ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b)  * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b)  * (p - y.c));
+
+        System.out.println("Area do triangulo X:" + areaX);
+        System.out.println("Area do triangulo Y:" + areaY);
+
+        sc.close();
+
     }
 }
